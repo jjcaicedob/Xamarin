725e5a28ccecb9666e2b24af56c5a5ed8ccb4439
John Jairo Caicedo Bolaños <john.caicedo.osc@gmail.com>
2017-03-07 05:54:35 -0500

    Versión inicial

---
+++ App1/App1/App1/Triangulo.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Triangulo : ContentPage
+    {
+        public Triangulo()
+        {
+            InitializeComponent();
+
+            btnCalcular.Clicked += BtnCalcular_Clicked;
+        }
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            if (entBase.Text == null)
+            {
+                DisplayAlert("Area del Triangulo", "El valor de la base no puede estar vacio.", "Aceptar");
+
+                return;
+            }
+
+            if (entAltura.Text == null)
+            {
+                DisplayAlert("Area del Triangulo", "El valor de la altura no puede estar vacio.", "Aceptar");
+
+                return;
+            }
+
+            double b = double.Parse(entBase.Text);
+            double h = double.Parse(entAltura.Text);
+
+            double a = b * h / 2;
+
+            lblResultado.Text = a.ToString();
+        }
+    }
+}
---
+++ AreaTriangulo/AreaTriangulo/AreaTriangulo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AreaTriangulo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnCalcular.Clicked += BtnCalcular_Clicked;
+        }
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            if (entBase.Text == null)
+            {
+                DisplayAlert("Area", "La base es obligatoria", "Aceptar");
+
+                return;
+            }
+
+            if (entAltura.Text == null)
+            {
+                DisplayAlert("Area", "La altura es obligatoria", "Aceptar");
+
+                return;
+            }
+
+            double b = double.Parse(entBase.Text);
+            double h = double.Parse(entAltura.Text);
+
+            double a = b * h / 2;
+
+            lblResultado.Text = a.ToString();
+        }
+    }
+}
